6930cfc7d8af19e7856796d80f329411192e378c
Артем Плотников <artemplotnikov1034@gmail.com>
2023-06-11 21:01:04 +0300

    Index and account templates added

--- FinanceManager/MoneyOperation/urls.py
+++ FinanceManager/MoneyOperation/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:account_name', views.account, name='account'),
+    path('<int:account_name>/transactions/', views.transactions, name='transactions'),
+    path('<int:account_name>/transactions/<int:transaction_id>', views.single_transaction, name='single_transaction'),
 ]
--- FinanceManager/MoneyOperation/views.py
+++ FinanceManager/MoneyOperation/views.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import UserAccount
 
 
 def index(request):
-    return HttpResponse("Salam, world. You're at the MoneyOperation index.")
+    account_list = UserAccount.objects.order_by('-registration_date')
+    return render(request, 'MoneyOperation/index.html', {'account_list': account_list})
 
 
-from django.shortcuts import render
+def account(request, account_name):
+    acc = get_object_or_404(UserAccount, pk=account_name)
+    return render(request, 'MoneyOperation/account.html', {'account': acc})
 
-# Create your views here.
+
+def transactions(request, account_name):
+    response = "You're looking at the transactions of account %s."
+    return HttpResponse(response % account_name)
+
+
+def single_transaction(request, account_name, transaction_id):
+    return HttpResponse("You're looking at the transaction %s." % transaction_id)
