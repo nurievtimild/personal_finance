e133f1daf70ba98eebfad0bc4e171f315e851c0b
Тимур Нуриев <nurievtimild@gmail.com>
2023-06-12 20:15:59 +0300

    подключили с димасом авторизацию пользователя и открытие профиля по результатам авторизации (login required). нужно допилить логику регистрации

--- budjet/polls/urls.py
+++ budjet/polls/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from . import views
+from .views import profile_view
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('registration/', views.registration, name='registration'),
     path('about/', views.about, name='about'),
-    path('login/', views.login, name='login')
+    # path('login/', views.login, name='login'),
+    path("profile/", profile_view, name='profile'),
 ]
